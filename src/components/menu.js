fc9bea0282f9c7a35e39b3fef92218f10a5f73b8
maya.pessel <maya.pessel@gmail.com>
2022-09-26 18:19:27 +0200

    Add navigation

---
+++ src/components/menu.js
@@ -0,0 +1,13 @@
+export default function Menu() {
+    return (
+        <section>
+            <nav className="menu shift">
+                <ul>
+                    <li><a href="">home</a></li>
+                    <li><a href="">add techno</a></li>
+                    <li><a href="">about</a></li>
+                </ul>
+            </nav>
+        </section>
+    )
+}
